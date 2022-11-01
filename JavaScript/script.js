1b56e8a6689d9dec11ed76e8f93572cc9da10210
Regis <antonioregis.contato+git@gmail.com>
2022-11-01 07:28:51 -0300

    search update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,16 @@
-meuStorage = localStorage;
+window.onload = () => {
+  const searchEl = document.querySelector('.search-input');
+  const x = document.querySelectorAll('.card-content h3');
+
+  function search(e){
+    x.forEach((item,index) => {
+      if(!item.innerHTML.toLowerCase().includes(e.target.value)){
+        item.parentElement.parentElement.style.display = 'none';
+      }else {
+        item.parentElement.parentElement.style.display = 'block';
+      }
+    })
+  };
+
+  searchEl.addEventListener('keyup', search)
+};
